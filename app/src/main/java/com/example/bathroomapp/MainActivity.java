a47dcd1ed70ea125913876df6fdbc31a40ccf5f4
mrswagbhinav <abhinav.vedmala@gmail.com>
2019-02-23 13:38:04 -0500

    Added getJSON()

--- app/src/main/java/com/example/bathroomapp/MainActivity.java
+++ app/src/main/java/com/example/bathroomapp/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //new getJSON().execute("http://api.openweathermap.org/data/2.5/forecast?zip=08852&appid=904eaa61902dba144dc85f29013ec210&units=imperial");
+
     }
 }
