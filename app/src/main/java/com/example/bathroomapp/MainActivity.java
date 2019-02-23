60cb0b5a30ef366f1f2ad2ac09de597055134575
mrswagbhinav <abhinav.vedmala@gmail.com>
2019-02-23 17:08:38 -0500

    Gets the data from the API

--- app/src/main/java/com/example/bathroomapp/MainActivity.java
+++ app/src/main/java/com/example/bathroomapp/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.bathroomapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,32 +28,93 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
 
-    String lat = "40.371680";
-    String lng = "-74.555990";
-    Boolean ada = false;
-    Boolean unisex = false;
+    double lat;
+    double lng;
+    String ada = "false";
+    String unisex = "false";
     JSONObject jsonObject;
     String s;
+
+    CheckBox isDisabled;
+    CheckBox isNeutral;
+    Button findBathroom;
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(ada && unisex)
-            new getJSON().execute("https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json?per_page=100&ada=true&unisex=true&lat="+lat+"&lng="+lng);
-        else if(!ada && !unisex)
-            new getJSON().execute("https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json?per_page=100&ada=false&unisex=false&lat="+lat+"&lng="+lng);
-        else if(ada && !unisex)
-            new getJSON().execute("https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json?per_page=100&ada=true&unisex=false&lat="+lat+"&lng="+lng);
-        else if(!ada && unisex)
-            new getJSON().execute("https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json?per_page=100&ada=false&unisex=true&lat="+lat+"&lng="+lng);
+        isDisabled = findViewById(R.id.id_isDisabled);
+        isNeutral = findViewById(R.id.id_isNeutral);
+        findBathroom = findViewById(R.id.id_findBathrooms);
+
+        intent = new Intent(this, LocationActivity.class);
+
+        isDisabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    ada = "true";
+                }
+                else
+                    ada = "false";
+            }
+        });
+
+        isNeutral.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    unisex = "true";
+                else
+                    unisex = "false";
+
+            }
+        });
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 200, 1, this);
+
+        findBathroom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new getJSON().execute("https://www.refugerestrooms.org:443/api/v1/restrooms/by_location.json?per_page=100&ada="+ada+"&unisex="+unisex+"&lat="+lat+"&lng="+lng);
+            }
+        });
 
     }
 
-    public class getJSON extends AsyncTask<String, Void, String> {
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+    private class getJSON extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... params) {
             try {
@@ -53,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Log.v("TAG", s);
+            Log.d("tag", s);
             return s;
         }
         @Override
@@ -61,7 +135,6 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(s);
             try {
                 jsonObject = new JSONObject(s);
-
             } catch (JSONException e) {
                 e.printStackTrace();
             }
